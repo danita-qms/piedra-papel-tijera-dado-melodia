3f932998ef4393a23c1542ddb647e571ca617e2b
danita-qms <80974110+danita-qms@users.noreply.github.com>
2021-03-19 10:15:33 -0600

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,92 @@
+let Dado = 0
+let mano = 0
+input.onButtonPressed(Button.A, function () {
+    Dado = randint(1, 6)
+    if (Dado == 1) {
+        basic.showLeds(`
+            . . # . .
+            . # # . .
+            . . # . .
+            . . # . .
+            . # # # .
+            `)
+    }
+    if (Dado == 2) {
+        basic.showLeds(`
+            . # # # .
+            . . . # .
+            . # # # .
+            . # . . .
+            . # # # .
+            `)
+    }
+    if (Dado == 3) {
+        basic.showLeds(`
+            . # # # .
+            . . . # .
+            . # # # .
+            . . . # .
+            . # # # .
+            `)
+    }
+    if (Dado == 4) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . # # # .
+            . . . # .
+            . . . # .
+            `)
+    }
+    if (Dado == 5) {
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . # # # .
+            . . . # .
+            . # # # .
+            `)
+    }
+    if (Dado == 6) {
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            `)
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    mano = randint(1, 3)
+    if (mano == 1) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            # # . # #
+            # # . # #
+            `)
+    }
+    if (mano == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    }
+    if (mano == 3) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
